f0169aa543cdf0590e036f7bc69fff770d38aa17
Ensar <ensarsu0220@gmail.com>
2023-05-21 15:51:58 +0200

    Added formater

--- components/Event card components/eventCard.js
+++ components/Event card components/eventCard.js
@@ -1,33 +1,32 @@
-import Styles from '../../styles/Event Card/eventCard.module.css';
-import Materials from '../../static API/event.json'
+import Styles from "../../styles/Event Card/eventCard.module.css";
+import Materials from "../../static API/event.json";
 
 export default function EventCard() {
+  return (
+    <>
+      {Materials.map((material) => {
+        return (
+          <div className={Styles.EventCardContainer} key={material.id}>
+            {/* Front Card */}
+            <div className={Styles.FrontContainer}>
+              <h2>{material.title}</h2>
+              <p>{material.text}</p>
 
-    return (
-        <>
-            {
-                Materials.map( material => {
-                    return(
-                        <div className={Styles.EventCardContainer} key={material.id}>
+              {/*Modal trigger */}
+              <button id="EventModalBtn" className={Styles.ModalButton}>
+                Read More
+              </button>
+            </div>
 
-                            {/* Front Card */}
-                            <div className={Styles.FrontContainer}>
-                                <h2>{material.title}</h2>
-                                <p>{material.text}</p>
-                
-                                {/*Modal trigger */}
-                                <button id="EventModalBtn" className={Styles.ModalButton}>Read More</button>
-                            </div>
-
-                            {/* Back Card */}
-                            <div className={Styles.BackContainer}>
-                                <p>{material.date.day}/{material.date.month}/{material.date.year}</p>
-                            </div>
-
-                        </div>
-                    )
-                })
-            }
-        </>
-    )
+            {/* Back Card */}
+            <div className={Styles.BackContainer}>
+              <p>
+                {material.date.day}/{material.date.month}/{material.date.year}
+              </p>
+            </div>
+          </div>
+        );
+      })}
+    </>
+  );
 }

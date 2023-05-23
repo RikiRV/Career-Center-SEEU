22c69d84caa190f177f13f8ff8866c7e7ff6c486
Ensar <ensarsu0220@gmail.com>
2023-05-23 23:33:26 +0200

    test

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,21 @@
-import PgHeader from "../components/Header/header"
-import '../styles/globals.css'
-import { Roboto } from 'next/font/google'
+import PgHeader from "../components/Header/header";
+import "../styles/globals.css";
+import { Roboto } from "next/font/google";
 import Footer from "../components/Footer Components/footer";
 
 const roboto = Roboto({
-    subsets: ['latin'],
-    weight: ['400'],
+  subsets: ["latin"],
+  weight: ["400"],
 });
 
-export default function App({ Component, pageProps}) {
-    return(
-        <main className={roboto.className}>
-            
-            <PgHeader/>
+export default function App({ Component, pageProps }) {
+  return (
+    <main className={roboto.className}>
+      <PgHeader />
 
-            <Component {...pageProps} />
-            
-            <Footer />
-            
-        </main>
-    )
+      <Component {...pageProps} />
+
+      <Footer />
+    </main>
+  );
 }

d975ff349702694cfc63d2b1fc12425a6bf3255b
Ensar <ensarsu0220@gmail.com>
2023-05-26 23:59:24 +0200

    Added languages | Contact us

--- components/Contact components/contactText.js
+++ components/Contact components/contactText.js
@@ -1,25 +1,35 @@
 import Styles from "../../styles/Contact/contactText.module.css";
-import Materials from "../../static API/ContactAPI/contactText.json";
+import al from "../../public/locales/al/ContactAL";
+import en from "../../public/locales/en/ContactEN";
+import mk from "../../public/locales/mk/ContactMK";
+import { useRouter } from "next/router";
 
 export default function ContactText() {
+  const router = useRouter();
+  const { locale } = router;
+
+  var t;
+
+  if (locale === "en") {
+    t = en;
+  } else if (locale === "al") {
+    t = al;
+  } else if (locale === "mk") {
+    t = mk;
+  }
+
   return (
     <>
-      {Materials.map((material) => {
-        return (
-          <>
-            <div className={Styles.TextContainer}>
-              <h1>{material.Title}</h1>
-              <p>{material.InformText}</p>
-              <p>{material.AltText}</p>
-            </div>
-            <div className={Styles.ContactContainer}>
-              <p>email: {material.Infromation.email}</p>
-              <p>Phone: {material.Infromation.PhoneNumber}</p>
-              <p>Location: {material.Infromation.Location}</p>
-            </div>
-          </>
-        );
-      })}
+      <div className={Styles.TextContainer}>
+        <h1>{t.Title}</h1>
+        <p>{t.InformText}</p>
+        <p>{t.AltText}</p>
+      </div>
+      <div className={Styles.ContactContainer}>
+        <p>email: {t.Infromation.email}</p>
+        <p>Phone: {t.Infromation.PhoneNumber}</p>
+        <p>Location: {t.Infromation.Location}</p>
+      </div>
     </>
   );
 }
--- components/Contact components/form.js
+++ components/Contact components/form.js
@@ -1,14 +1,30 @@
 import Styles from "../../styles/Contact/form.module.css";
+import al from "../../public/locales/al/ContactAL";
+import en from "../../public/locales/en/ContactEN";
+import mk from "../../public/locales/mk/ContactMK";
+import { useRouter } from "next/router";
 
 export default function Form() {
+  const router = useRouter();
+  const { locale } = router;
+
+  var t;
+
+  if (locale === "en") {
+    t = en;
+  } else if (locale === "al") {
+    t = al;
+  } else if (locale === "mk") {
+    t = mk;
+  }
   return (
     <div className={Styles.formContainer}>
       <form action="https://formspree.io/f/xoqzzqan" method="POST">
-        <label>Full Name:</label>
+        <label>{t.name}</label>
         <input type="text" name="name" required></input>
-        <label>Your email:</label>
+        <label>{t.email}</label>
         <input type="email" name="email" required></input>
-        <label>Your message:</label>
+        <label>{t.message}</label>
         <textarea name="message" required></textarea>
         <button type="submit">Send</button>
       </form>

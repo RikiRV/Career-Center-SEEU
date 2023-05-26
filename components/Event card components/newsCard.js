d273fc57b8feb050224342539d0e605e88a2a2f1
Ensar <ensarsu0220@gmail.com>
2023-05-27 00:12:12 +0200

    Added languages | news

--- components/Event card components/newsCard.js
+++ components/Event card components/newsCard.js
@@ -1,10 +1,25 @@
 import Styles from "../../styles/Event Card/newsCard.module.css";
-import Materials from "../../static API/news.json";
+import { useRouter } from "next/router";
+import al from "../../public/locales/al/NewsAL";
+import en from "../../public/locales/en/NewsEN";
+import mk from "../../public/locales/mk/NewsMK";
 
 export default function NewsCard() {
+  const router = useRouter();
+  const { locale } = router;
+
+  var t;
+
+  if (locale === "en") {
+    t = en;
+  } else if (locale === "al") {
+    t = al;
+  } else if (locale === "mk") {
+    t = mk;
+  }
   return (
     <>
-      {Materials.map((material) => {
+      {t.map((material) => {
         return (
           <div className={Styles.NewsCardContainer} key={material.id}>
             {/* Front Card */}
---
+++ public/locales/en/NewsEN.js
@@ -0,0 +1,16 @@
+export default [
+  {
+    id: 1,
+    NewsTitle: "Our news for today EN",
+    NewsContent:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to",
+    ImgLink: "News Photo.jpg",
+  },
+  {
+    id: 2,
+    NewsTitle: "Our news for today",
+    NewsContent:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to",
+    ImgLink: "News Photo.jpg",
+  },
+];

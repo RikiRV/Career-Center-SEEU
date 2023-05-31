ddeeff545e23abee22ebf6c9f4763b78770810ea
Ensar <ensarsu0220@gmail.com>
2023-05-31 14:20:20 +0200

    Before Adding material from document

--- public/locales/al/ContactAL.js
+++ public/locales/al/ContactAL.js
@@ -1,7 +1,7 @@
 export default {
   Title: "How to contact us AL",
   InformText:
-    "If you have a concerne or a short message you would like for us to recieve, use the form below to send us a direct message",
+    "If you have a concern or a short message you would like for us to receive, use the form below to send us a direct message",
   AltText:
     "If you have more on your mind, then feel free to send us an email, give us a call or come visit us at our location.",
   Infromation: {
